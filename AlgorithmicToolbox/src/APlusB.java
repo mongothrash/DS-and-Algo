d6d4e922a99a0d90ae89327836456ed23ab1033d
Bradly Sherk <bradlysherk@gmail.com>
2016-03-15 12:11:53 -0700

    First assignment

---
+++ AlgorithmicToolbox/src/APlusB.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class APlusB {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        s.close();
+        System.out.println(a + b);
+        System.out.println("BRAD!");
+    }
+}
